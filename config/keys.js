ddc5da8a2bc32c501d7052333be5e3326de923cc
Joe <luqiao19930111@gmail.com>
2020-12-18 18:25:11 -0500

    Finished OAuth flow

---
+++ config/keys.js
@@ -0,0 +1,11 @@
+// key.js - figure out what set of config to return
+
+if (process.env.NODE_ENV == 'production') {
+    // We are in production - return the prod.js keys
+    module.exports = require('./prod');
+} else {
+    // We are in production - return the dev.js keys
+    module.exports = require('./dev');
+}
+
+
---
+++ config/prod.js
@@ -0,0 +1,7 @@
+// dev.js - development keys.
+module.exports = {
+    googleClientID: process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.GOOGLE.CLIENT_SECRET,
+    mongoURI: process.env.MONGO_URI,
+    cookieKey: process.env.COOKIE_KEY 
+}
