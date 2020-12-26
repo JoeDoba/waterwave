30b80abadc761c6e098cd91945d2fcad11623708
Joe <luqiao19930111@gmail.com>
2020-12-25 20:30:23 -0500

    deploy modify3

--- config/prod.js
+++ config/prod.js
@@ -1,11 +1,13 @@
-// dev.js - development keys.
 module.exports = {
     googleClientID: process.env.GOOGLE_CLIENT_ID,
     googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+
     githubClientID: process.env.GITHUB_CLIENT_ID,
     githubClientSecret: process.env.GITHUB_CLIENT_SECRET,
+
     mongoURI: process.env.MONGO_URI,
     cookieKey: process.env.COOKIE_KEY,
+
     stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
     stripeSecretKey: process.env.STRIPE_SECRET_KEY, 
 }
