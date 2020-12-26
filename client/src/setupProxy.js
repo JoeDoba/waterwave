742da5c7c8f3ebf706be05d317b3a4c1fc874b9d
Joe <luqiao19930111@gmail.com>
2020-12-25 21:11:55 -0500

    deploy modify5

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,8 +1,8 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = function (app) {
-  // if (process.env.NODE_ENV === 'production') {
-  //   return;
-  // }
+  if (process.env.NODE_ENV === 'production') {
+    return;
+  }
 
   app.use(
     ["/api/*", "/auth/google", "/auth/github"],
