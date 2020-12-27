11ff5b2a0710e1e0b8c0243355c5f3b5aff28e93
Joe <luqiao19930111@gmail.com>
2020-12-27 13:37:03 -0500

    add survey and recipient modeel

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 const keys = require('./config/keys');
 
 require('./models/user');
+require('./models/survey')
 require('./services/passpost');
 
 mongoose.connect(keys.mongoURI);
